8dfae047a8c1646d0cc3ac022a8fc886c36e2519
huangHIT <hyf3513onego@gmail.com>
2024-10-03 21:55:55 +0800

    Answer to Q4

--- src/App.jsx
+++ src/App.jsx
@@ -72,24 +72,69 @@ class Display extends React.Component {
 }
 
 class Add extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePassport = this.handlePassport.bind(this);
+    this.handleID = this.handleID.bind(this);
+    this.handlePhone = this.handlePhone.bind(this);
+    this.handleName = this.handleName.bind(this)
+    this.handleDate = this.handleDate.bind(this)
+    this.state = {ID:"",name:"",phone:111111,passportID:"A1111",bookingTimeStr:"",bookingTime:new Date(),msg:""}
   }
-
   handleSubmit(e) {
     e.preventDefault();
+    let {flag,errorMsg}=this.props.bookTraveller({ID:this.state.ID,name:this.state.name,phone:this.state.phone,passportID:this.state.passportID,bookingTime:this.state.bookingTime});
     /*Q4. Fetch the passenger details from the add form and call bookTraveller()*/
   }
-
+  handleID(e){
+    this.setState({ID: e.target.value});
+  }
+  handleName(e){
+    this.setState({name: e.target.value});
+  }
+  handlePhone(e){
+    this.setState({phone: Number(e.target.value)});
+  }
+  handlePassport(e){
+    this.setState({passportID: e.target.value});
+  }
+  handleDate(e){
+    this.setState({bookingTimeStr:e.target.value});
+    this.setState({bookingTime:new Date(e.target.value)});
+  }
   render() {
     return (
-      <form name="addTraveller" onSubmit={this.handleSubmit}>
-	    {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
-        <h2>Add Page</h2>
-        <input type="text" name="travellername" placeholder="Name" />
-        <button>Add</button>
-      </form>
+        <form name="addTraveller" onSubmit={this.handleSubmit}>
+          {/*Q4. Placeholder to enter passenger details. Below code is just an example.*/}
+          <h2>Add Page</h2>
+          <div>
+            <label className={"formLabel"}>ID:</label>
+            <input onChange={this.handleID} value={this.state.ID} type="text" name="travellerID" placeholder="ID"/>
+          </div>
+          <div>
+            <label className={"formLabel"}>Name:</label>
+            <input onChange={this.handleName} value={this.state.name} type="text" name="travellerName"
+                   placeholder="Name"/>
+          </div>
+          <div>
+            <label className={"formLabel"}>Phone:</label>
+            <input onChange={this.handlePhone} value={this.state.phone} type="text" name="travellerPhone"
+                   placeholder="phone"/>
+          </div>
+          <div>
+            <label className={"formLabel"}>Passport ID:</label>
+            <input onChange={this.handlePassport} value={this.state.passportID} type="text" name="travellerPassport"
+                   placeholder="passport"/>
+          </div>
+          <div>
+            <label className={"formLabel"}>Date:</label>
+            <input onChange={this.handleDate} value={this.state.bookingTimeStr} type="date" name="travellerDate"
+                   placeholder="2024-01-01"/>
+          </div>
+          <button>Add</button>
+        </form>
     );
   }
 }
@@ -100,7 +145,7 @@ class Delete extends React.Component {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.state = {nameInput:"",msg:""}
+    this.state = {nameInput: "", msg:""}
   }
   handleSubmit(e) {
     e.preventDefault();
@@ -174,7 +219,7 @@ class Homepage extends React.Component {
 class TicketToRide extends React.Component {
   constructor() {
     super();
-    this.state = { travellers: [], selector: 3,totalSeatsNum:0};
+    this.state = { travellers: [], selector: 1,totalSeatsNum:0};
     this.bookTraveller = this.bookTraveller.bind(this);
     this.deleteTraveller = this.deleteTraveller.bind(this);
   }
@@ -196,6 +241,15 @@ class TicketToRide extends React.Component {
 
   bookTraveller(passenger) {
 	    /*Q4. Write code to add a passenger to the traveller state variable.*/
+    let newTravellers = [
+        ...this.state.travellers,
+      {id: passenger.ID, name:passenger.name, phone: passenger.phone,passportID:passenger.passportID,
+      bookingTime: passenger.bookingTime}
+    ]
+    let flag = true
+    let msg = ""
+    this.setState({travellers:newTravellers})
+    return {flag,msg}
   }
 
   deleteTraveller(passenger) {
@@ -234,7 +288,7 @@ class TicketToRide extends React.Component {
 		{/*Only one of the below four divisions is rendered based on the button clicked by the user.*/}
 		{/*Q2 and Q6. Code to call Instance that draws Homepage. Homepage shows Visual Representation of free seats.*/}
       {this.state.selector===0?<Homepage travellers={this.state.travellers} totalSeatsNum={this.state.totalSeatsNum}></Homepage>:null}
-      {this.state.selector===1?<Add></Add>:null}
+      {this.state.selector===1?<Add bookTraveller={this.bookTraveller} travellers={this.state.travellers}></Add>:null}
       {this.state.selector===2?<Delete deleteTraveller={this.deleteTraveller} travellers={this.state.travellers}></Delete>:null}
       {this.state.selector===3?<Display travellers={this.state.travellers} deleteTraveller={this.deleteTraveller}></Display>:null}
 		{/*Q3. Code to call component that Displays Travellers.*/}
